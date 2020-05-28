b9ec104962455cdd3e840e17fb1c044ef4b562b4
AwesomeCronk <awesomecronk@gmail.com>
2020-05-27 19:15:21 -0500

    Committing for backup.

--- logic.py
+++ logic.py
@@ -1,10 +1,33 @@
-class _gate():
-    gateType = 'none'
+class _input():
+    value = False
+    reference = None
 
-    def __init__(self, gateType):
+    def __init__(self):
+        pass
+
+    def point(self, target):
+        if type(target) is not _output:
+            return False
+        self.reference = target
+
+    def fetch(self):
+        self.value = self.reference.value
+        return self.value
+
+class _output():
+    value = False
+
+    def __init__(self):
+        pass
+
+    def set(self, newValue):
+        self.value = bool(newValue)
+        
+class _gate():
+    def __init__(self):
+        print('initializing gate {}'.format(self))
         self._in = [_input(), _input()]
         self._out = _output()
-        self.gateType = gateType
 
 #    def config(self, gateType, inputTargets):
 #        #just some safety checks
@@ -32,27 +55,17 @@ class _gate():
             else:
                 self._out.set(False)
 
-class _input():
-    value = False
-    reference = None
+        elif self.gateType == 'or':
+            if self._in[0].fetch() and not self._in[1].fetch():
+                self._out.set(True)
+            elif not self._in[0].fetch() and self._in[1].fetch():
+                self._out.set(True)
+            elif self._in[0].fetch() and self._in[1].fetch():
+                self._out.set(True)
+            else:
+                self._out.set(False)
 
+class _andGate(_gate):
     def __init__(self):
-        pass
-
-    def point(self, target):
-        if type(target) is not _output:
-            return False
-        self.reference = target
-
-    def fetch(self):
-        self.value = self.reference.value
-        return self.value
-
-class _output():
-    value = False
-
-    def __init__(self):
-        pass
-
-    def set(self, newValue):
-        self.value = bool(newValue)
+        _gate.__init__(self)                #I may decide to switch one of these in the future
+        #super(_gate, self).__init__(self)
