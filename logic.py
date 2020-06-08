9701ad6ab39985745ef31e1b9726337c151bd7c4
AwesomeCronk <awesomecronk@gmail.com>
2020-06-08 07:20:10 -0500

    gInput, gOutput, gAndGate, and gOrGate are working. Added notes.txt to keep track of things. Did some general cleanup.

--- logic.py
+++ logic.py
@@ -5,7 +5,7 @@ class _input():
     def __init__(self):
         pass
 
-    def point(self, target):
+    def connect(self, target):
         if type(target) is not _output:
             return False
         self.reference = target
@@ -29,43 +29,28 @@ class _gate():
         self._in = [_input(), _input()]
         self._out = _output()
 
-#    def config(self, gateType, inputTargets):
-#        #just some safety checks
-#        if gateType not in supportedGates:
-#            return False
-#   
-#        self.gateType = gateType
-#        if type(inputTargets) is not list:
-#            return False
-#
-#        for i in inputTargets:
-#            if type(i) is not output:
-#                return False
-#        
-#        self.gateType = gateType
-#        for i in range(len(self._in)):
-#            self._in[i].point(inputTargets[i])
-
-    def update(self):
-        print("update for {} of type '{}'".format(self, self.gateType))
-
-        if self.gateType == 'and':
-            if self._in[0].fetch() and self._in[1].fetch():
-                self._out.set(True)
-            else:
-                self._out.set(False)
-
-        elif self.gateType == 'or':
-            if self._in[0].fetch() and not self._in[1].fetch():
-                self._out.set(True)
-            elif not self._in[0].fetch() and self._in[1].fetch():
-                self._out.set(True)
-            elif self._in[0].fetch() and self._in[1].fetch():
-                self._out.set(True)
-            else:
-                self._out.set(False)
-
 class _andGate(_gate):
     def __init__(self):
-        _gate.__init__(self)                #I may decide to switch one of these in the future
+        _gate.__init__(self)                #I may decide to switch these in the future
         #super(_gate, self).__init__(self)
+
+    def update(self):
+        if self._in[0].fetch() and self._in[1].fetch():
+            self._out.set(True)
+        else:
+            self._out.set(False)
+            
+class _orGate(_gate):
+    def __init__(self):
+        _gate.__init__(self)                #I may decide to switch these in the future
+        #super(_gate, self).__init__(self)
+
+    def update(self):
+        if self._in[0].fetch() and not self._in[1].fetch():
+            self._out.set(True)
+        elif not self._in[0].fetch() and self._in[1].fetch():
+            self._out.set(True)
+        elif self._in[0].fetch() and self._in[1].fetch():
+            self._out.set(True)
+        else:
+            self._out.set(False)
--- main.py
+++ main.py
@@ -1,62 +1,76 @@
 try:
     from PyQt5 import QtWidgets
-    from PyQt5.QtWidgets import QApplication, QMainWindow, QAbstractButton
+    from PyQt5.QtWidgets import QApplication, QMainWindow, QAbstractButton, QPushButton, QLabel
     from PyQt5.QtGui import QPixmap, QPainter
 except:
     input('Error importing PyQt5 components. (press enter to close)')
     exit()
 
-from logic import _gate, _input, _output
+from logic import _input, _output, _andGate, _orGate
 
 class mainWindow(QMainWindow):
-    gates = []
-    
     def __init__(self):
         super(mainWindow, self).__init__()
         print('starting...')
-        
-        g = gGate(self, "./images/gates/andRaw.png")
-        g.clicked.connect(self.newAndGate)
+        self.setGeometry(200, 500, 800, 500)
+        self.objects = []
+        self.generateStaticControls()
+        self.testObjects()
         #with open('test.lgc', 'r') as f:
          #   data = f.read()
           #  print(data)
            # f.close()
 
-    def testGates(self):
-        andGate = _gate('and')
-        in1 = gInput()
-        in2 = gInput()
-        out1 = gOutput()
+    def generateStaticControls(self):
+        self.newAndButton = QPushButton(self)
+        self.newAndButton.setText('and')
+        self.newAndButton.setGeometry(10, 10, 40, 20)
+        self.newAndButton.clicked.connect(self.newAndGate)
 
-        andGate._in[0].point(in1._out)
-        andGate._in[1].point(in2._out)
-        out1._in.point(andGate._out)
+        self.newOrButton = QPushButton(self)
+        self.newOrButton.setText('or')
+        self.newOrButton.setGeometry(10, 40, 40, 20)
+        self.newOrButton.clicked.connect(self.newOrGate)
+
+        self.newXorButton = QPushButton(self)
+        self.newXorButton.setText('xor')
+        self.newXorButton.setGeometry(10, 70, 40, 20)
+        self.newXorButton.clicked.connect(self.newXorGate)
+
+        self.newNandButton = QPushButton(self)
+        self.newNandButton.setText('nand')
+        self.newNandButton.setGeometry(10, 100, 40, 20)
+        self.newNandButton.clicked.connect(self.newNandGate)
+
+        self.newNorButton = QPushButton(self)
+        self.newNorButton.setText('nor')
+        self.newNorButton.setGeometry(10, 130, 40, 20)
+        self.newNorButton.clicked.connect(self.newNorGate)
+
+        self.newXnorButton = QPushButton(self)
+        self.newXnorButton.setText('xnor')
+        self.newXnorButton.setGeometry(10, 160, 40, 20)
+        self.newXnorButton.clicked.connect(self.newXnorGate)
         
-        andGate.update()
-        out1.update()
+        self.newNotButton = QPushButton(self)
+        self.newNotButton.setText('not')
+        self.newNotButton.setGeometry(10, 190, 40, 20)
+        self.newNotButton.clicked.connect(self.newNotGate)
 
-        in1.toggle()
-        andGate.update()
-        out1.update()
-
-        in1.toggle()
-        in2.toggle()
-        andGate.update()
-        out1.update()
-
-        in1.toggle()
-        andGate.update()
-        out1.update()
+        self.updateButton = QPushButton(self)
+        self.updateButton.setText('Update')
+        self.updateButton.setGeometry(730, 470, 60, 20)
+        self.updateButton.clicked.connect(self.update)
 
     def update(self):
-        #needsUpdate = True
+        #needsUpdate = True     #Theory for a system that updates as needed instead of a fixed number of times per loop. Potential issues with memory-type circuits.
         #states = []
         #newstates = []
         #while needsUpdate:
         #    needsUpdate = False
         for i in range(5):
-            for g in self.gates:
-                g.update()
+            for o in self.objects:
+                o.update()
 
     def initUI(self):
         newAndBtn = QPushButton(self)
@@ -65,14 +79,165 @@ class mainWindow(QMainWindow):
         newInBtn = QPushButton(self)
         newOutBtn = QPushButton(self)
 
+    def newInput(self):
+        print('new input')
+
+    def newOutput(self):
+        print('new output')
+
+    #and, or, xor, nand, nor, xnor, not
     def newAndGate(self):
-        print("new and gate")
+        print("new andGate")
+
+    def newOrGate(self):
+        print("new orGate")
+
+    def newXorGate(self):
+        print("new xorGate")
+
+    def newNandGate(self):
+        print("new nandGate")
+
+    def newNorGate(self):
+        print("new norGate")
+
+    def newXnorGate(self):
+        print("new xnorGate")
+
+    def newNotGate(self):
+        print("new notGate")
+
+    def testGates(self):
+        andGate = _andGate()
+        orGate = _orGate()
+        in1 = gInput(self)
+        in2 = gInput(self)
+        out1 = gOutput(self)
+        out2 = gOutput(self)
+
+        andGate._in[0].connect(in1._out)
+        andGate._in[1].connect(in2._out)
+        out1._in.connect(andGate._out)
+        
+        orGate._in[0].connect(in1._out)
+        orGate._in[1].connect(in2._out)
+        out2._in.connect(orGate._out)
+
+        andGate.update()    #0, 0
+        orGate.update()
+        out1.update()
+        out2.update()
+
+        in2.toggle()
+        andGate.update()    #0, 1
+        orGate.update()
+        out1.update()
+        out2.update()
+
+        in1.toggle()        #1, 0
+        in2.toggle()
+        andGate.update()
+        orGate.update()
+        out1.update()
+        out2.update()
+
+        in2.toggle()        #1, 1
+        andGate.update()
+        orGate.update()
+        out1.update()
+        out2.update()
+
+    def testObjects(self):
+        in1 = gInput(self)
+        in2 = gInput(self)
+        and1 = gAndGate(self)
+        out1 = gOutput(self)
+        in1.move(60, 10)
+        in2.move(60, 40)
+        and1.move(90, 10)
+        out1.move(150, 25)
+
+    def loadProgram(self, programPath):
+        programCommands = []
+        with open(programPath, 'r') as programFile:
+            loop = True
+            while loop:
+                data = programFile.readline()
+                if data:
+                    programCommands.append(data)
+                else:
+                    print('Finished reading program file.')
+                    loop = False
+        return programCommands
+        
+    def runProgram(self, commands: list):
+        nameComp = {}
+        for c in commands:
+            data = c.split()
+            if data[0] == 'INPUT':
+                self.newInput()
+
+# Object definitions:
+
+class gInput(QPushButton):
+    xSize = 20
+    ySize = 20
+    def __init__(self, parent):
+        self.parent = parent
+        QPushButton.__init__(self, self.parent)
+        self.clicked.connect(self.toggle)
+        self.value = False
+        self._out = _output()
+        self.setText(str(int(self.value)))
+
+    def toggle(self):
+        self.value = not self.value
+        self._out.set(self.value)
+        self.setText(str(int(self.value)))
+
+    def move(self, xPos, yPos):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.setGeometry(self.xPos, self.yPos, self.xSize, self.ySize)
+
+    def update(self):
+        pass
+
+class gOutput(QLabel):
+    xSize = 20
+    ySize = 20
+    styleSheet = """background-color: rgb(225, 225, 225);\n
+        border: 1px solid rgb(173, 173, 173);\n
+        text-align: center;"""
+
+    def __init__(self, parent):
+        self.parent = parent
+        QLabel.__init__(self, self.parent)
+        self.value = False
+        self._in = _input()
+        self.setText(str(int(self.value)))
+        self.setStyleSheet(self.styleSheet)
+
+    def connect(self, target):
+        self._in.connect(target)
+
+    def move(self, xPos, yPos):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.setGeometry(self.xPos, self.yPos, self.xSize, self.ySize)
+
+    def update(self):
+        self.value = self._in.fetch()
+        self.setText(str(int(self.value)))
+        print('update for {} with value {}'.format(self, self.value))
 
 class gGate(QAbstractButton):
-    loc = (0, 0)
+    xSize = 50
+    ySize = 50
     def __init__(self, parent, imagePath):
         self.parent = parent
         self.imagePath = imagePath
+        self.loc = (0, 0)
         super(gGate, self).__init__(self.parent)
         self.pixMap = QPixmap(imagePath)
         self.setGeometry(self.loc[0], self.loc[1], 50, 50)
@@ -84,23 +249,28 @@ class gGate(QAbstractButton):
     def sizeHint(self):
         return self.pixMap.size()
 
-class gInput():
-    def __init__(self):
-        self.value = False
-        self._out = _output()
+    def move(self, xPos, yPos):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.setGeometry(self.xPos, self.yPos, self.xSize, self.ySize)
 
-    def toggle(self):
-        self.value = not self.value
-        self._out.set(self.value)
+class gAndGate(_andGate, gGate):
+    def __init__(self, parent):
+        self.parent = parent
+        self.imagePath = 'images/gates/andRaw.svg'
+        gGate.__init__(self, self.parent, self.imagePath)
+        _andGate.__init__(self)
 
-class gOutput():
-    def __init__(self):
-        self.value = False
-        self._in = _input()
+class gOrGate(_orGate, gGate):
+    def __init__(self, parent):
+        self.parent = parent
+        self.imagePath = 'images/gates/orRaw.svg'
+        gGate.__init__(self, self.parent, self.imagePath)
+        _andGate.__init__(self)
 
-    def point(self, target):
-        self._in.point(target)
-
-    def update(self):
-        self.value = self._in.fetch()
-        print('update for {} with value {}'.format(self, self.value))
+#class gXorGate(_xorGate, gGate):
+ #   def __init__(self, parent):
+  #      self.parent = parent
+   #     self.imagePath = 'images/gates/xorRaw.svg'
+    #    gGate.__init__(self, self.parent, self.imagePath)
+     #   _andGate.__init__(self)
