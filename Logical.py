8364cd0c2a6dc869399a7b578d504b625e17f8f0
Clayton Cronk <awesomecronk@gmail.com>
2021-12-26 21:34:31 -0600

    Started moving the whole deal over to the stdin key event system. Need to get element keyEvents figured out.

--- Logical.py
+++ Logical.py
@@ -7,11 +7,10 @@ import simpleANSI as ANSI
 
 from loading.loading import loadElement, setDebug
 from ui import vec2, widget, ansiManager, initANSI, cleanupANSI
+from keys import createKeyEvent, keyEvents, pollKeyEvents
 
 sys.stdout = io.TextIOWrapper(open(sys.stdout.fileno(), 'wb', 0), write_through=True)
 sys.stdout.write('=====test=====')
-# Info on pynput: https://pynput.readthedocs.io/en/latest/keyboard.html
-# Checking terminal activity status: https://unix.stackexchange.com/a/480138/496091
 
 with open(os.path.join(os.path.dirname(__file__), 'Logical.version.txt'), 'r') as file:
     version = file.read()
@@ -45,25 +44,6 @@ def getArgs():
 
     return parser, parser.parse_args()
 
-class keyTester():
-    def __init__(self):
-        print('Press any key to see the text for that should be used in Logical code for key bindings. Press Esc to exit.')
-    
-        keyListener = keyboard.Listener(
-                    on_press=self.keyPress,
-                    on_release=self.keyRelease
-                )
-        keyListener.start()
-        keyListener.join()
-
-    def keyPress(self, key):
-        print(str(key))
-        if key == keyboard.Key.esc:
-            return False
-
-    def keyRelease(self, key):
-        pass
-
 class keyBoardListenerManager():
     def __init__(self, simulation):
         self.simulation = simulation
@@ -82,14 +62,8 @@ class simulation():
     def __init__(self, filePath, verbose):
         self.runFlag = True
         self.simRunFlag = True
-        self.breakpointFlag = False
-        self.altHeld = False
-        self.altKeys = [keyboard.Key.alt,
-            keyboard.Key.alt_gr,
-            keyboard.Key.alt_l,
-            keyboard.Key.alt_r
-        ]
-        self.keyBindsToExecute = []
+        # self.breakpointFlag = False     # Reserved for breakpoint() feature
+        self.eventsToCall = []
         self.verbose = verbose
 
         setDebug(self.verbose)
@@ -97,13 +71,12 @@ class simulation():
         if self.verbose:
             input('Press enter to continue...')
 
-        self.titleString = 'Logical {} (Python {} on {} {}): {}'.format(version, pythonVersion, architecture, platform, filePath.replace('\\', '/').split('/')[-1])
+        self.titleString = 'Logical {} (Python {} on {} {}): {}'.format(version, pythonVersion, platform, architecture, filePath.replace('\\', '/').split('/')[-1])
         self.initUI()
-        self.keyListener = keyboard.Listener(
-            on_press=self.keyPress,
-            on_release=self.keyRelease
-        )
-        self.keyListener.start()
+        
+        createKeyEvent('\\x7f', self.togglePaused, self)    # Backspace
+        createKeyEvent('\\x1b', self.exit, self)    # Esc
+        createKeyEvent('\\x03', self.exit, self)    # Ctrl+C
 
     def initUI(self):
         print(ANSI.clear.entireScreen(), end = '')
@@ -127,92 +100,55 @@ class simulation():
         self.updateDebug.moveTo(vec2(0, terminalSize[1] - 3))
         self.mainWidget.addWidget(self.updateDebug)
 
-    def togglePause(self)
-
-    def keyPress(self, key):
-        if not self.runFlag:
-            return False
-        if not self.breakpointFlag:
-            with keyBoardListenerManager(self):     # Prevent hanging threads
-                # If this console is not the active window then return
-                if not checkTerminalActive:
-                    return
-                
-                if self.verbose:
-                    self.keyListenerDebug.setText(str(key))
-                    self.keyListenerDebug.setText(str(key) + ' - ' + str(self.mainElement.keyBinds))
-
-                # Backspace to toggle simulation
-                if key == keyboard.Key.backspace:
-                    if self.verbose:
-                        backspaceString = 'backspace pressed'
-                        self.keyListenerDebug.setText(backspaceString + ' ' * (terminalSize[0] - len(backspaceString)))
-                    self.simRunFlag = not self.simRunFlag
-                    if self.simRunFlag:
-                        self.title.setText(self.titleString + '         ')
-                    else:
-                        self.title.setText(self.titleString + ' (paused)')
-
-                # Escape to exit
-                elif key == keyboard.Key.esc:
-                    if self.verbose:
-                        self.keyListenerDebug.setText('escape pressed                     ')
-                    self.mainWidget.update()    # One last update to see if it died right
-                    self.runFlag = False
-                    return False
-
-                # Insert to break into debugger
-                elif key == keyboard.Key.insert:
-                    self.breakpointFlag = True
-
-                # Set the appropriate keybinds
-                elif str(key) in self.mainElement.keyBinds.keys():
-                    if self.verbose:
-                        self.keyListenerDebug.setText('{} found in {}'.format(str(key), str(self.mainElement.keyBinds)))
-                    for f in self.mainElement.keyBinds[str(key)]:
-                        self.keyBindsToExecute.append((f, True))
-
-                else:
-                    if self.verbose:
-                        self.keyListenerDebug.setText('{} not found in {}'.format(str(key), str(self.mainElement.keyBinds)))
-
-    def keyRelease(self, key):
-        if not self.runFlag:
-            return False
-        if not self.breakpointFlag:
-            with keyBoardListenerManager(self):
-                # Set the appropriate keybinds
-                if str(key) in self.mainElement.keyBinds.keys():
-                    for f in self.mainElement.keyBinds[str(key)]:
-                        self.keyBindsToExecute.append((f, False))
+    def togglePaused(self, state):
+        # Check the state here because we don't want the key release to toggle the pause
+        if state:
+            self.simRunFlag = not self.simRunFlag
+            if self.simRunFlag:
+                self.title.setText(self.titleString + '         ')
+            else:
+                self.title.setText(self.titleString + ' (paused)')
+    
+    def exit(self, state):
+        # Here we don't care about state since it's a one-time gig
+        self.mainWidget.update()    # One last update to see if it died right
+        self.runFlag = False
 
     def main(self):
-        updateTime = 0
-        while self.runFlag:
-            startTime = process_time_ns()
-            self.updateDebug.setText('Update time: {}us   '.format(str(updateTime).rjust(7)))
-            
-            if self.breakpointFlag:
-                breakpoint()
-                print(ANSI.clear.entireScreen(), end='')
-                self.breakpointFlag = False
+        with pollKeyEvents(self):
+            updateTime = 0
+            while self.runFlag:
+                startTime = process_time_ns()
+                self.updateDebug.setText('Update time: {}us   '.format(str(updateTime).rjust(7)))
+                
+                # May re-implement if I find a use for it
+                # Insert creates a sequence of keys and breakpoint() is useless in the compiled version
+                # if self.breakpointFlag:
+                #     breakpoint()
+                #     print(ANSI.clear.entireScreen(), end='')
+                #     self.breakpointFlag = False
 
-            if self.simRunFlag:
-                # Execute all the key binding functions set during the last update cycle
-                # Prevents contentions from threading
-                for f, state in self.keyBindsToExecute:
-                    f(state)
-                    self.keyBindsToExecute.remove((f, state))
+                # Uncall & call all necessary key events
+                for currentEvent in keyEvents.values():
+                    currentEvent.unCall()   # This is uncalled for...
+                for func, state in self.eventsToCall:
+                    func(state)
+                    del(self.eventsToCall[0])
 
-                self.mainElement.preUpdate()    # Fetch pin states before they change
-                self.mainElement.update()   # Update each element
-            else:
-                time.sleep(0.2) # Chillax for a split second, saves the CPU
+                if self.simRunFlag:
+                    # Execute all the key binding functions set during the last update cycle
+                    # Prevents contentions from threading
+                    for f, state in self.keyBindsToExecute:
+                        f(state)
+                        self.keyBindsToExecute.remove((f, state))
 
-            self.mainWidget.update()    # Update each widget
-            updateTime = int((process_time_ns() - startTime) / 1000)
-    
-        self.keyListener.join()
+                    self.mainElement.preUpdate()    # Fetch pin states before they change
+                    self.mainElement.update()   # Update each element
+                else:
+                    time.sleep(0.2) # Chillax for a split second, saves the CPU
+
+                self.mainWidget.update()    # Update each widget
+                updateTime = int((process_time_ns() - startTime) / 1000)
 
     def exit(self):
         self.runFlag = False
